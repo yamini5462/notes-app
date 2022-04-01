01d0724de7f11343212bc9256e6937df325e9c25
yamini <yamini.rangisetty1@ibm.com>
2022-04-01 17:18:50 +0530

    first commit

---
+++ Person.js
@@ -0,0 +1,8 @@
+const name="Hema"
+module.exports.name=name;
+
+function getPerson(){
+    return name;
+
+}
+module.exports.getPerson=getPerson;
---
+++ notes.js
@@ -0,0 +1,72 @@
+const fs = require('fs')
+const chalk= require('chalk')
+const getNotes = function () {
+    return 'Your notes...'
+}
+
+const addNote = function (title, body) {
+    const notes = loadNotes()
+    const duplicateNotes = notes.filter(function (note) {
+        return note.title === title
+    })
+
+    if (duplicateNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+        console.log('New note added!')
+    } else {
+        console.log('Note title taken!')
+    }
+}
+const removeNote = function (title) {
+    const notes = loadNotes()
+    const notesToKeep = notes.filter(function (note) {
+        return note.title !== title
+    })
+
+    if (notes.length > notesToKeep.length) {
+        console.log(chalk.green.inverse('Note removed!'))
+        saveNotes(notesToKeep)
+    } else {
+        console.log(chalk.red.inverse('No note found!'))
+    }    
+}
+const saveNotes = function (notes) {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+}
+
+const loadNotes = function () {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (e) {
+        return []
+    }
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote,
+    removeNote: removeNote
+}
+
+
+
+
+    // const person=require("./Person.js");
+// fs.writeFileSync('notes.txt','this is first Notes')
+// fs.appendFileSync('notes.txt',' \nthis is second Notes')
+
+
+// console.log(person.name);
+// console.log( "my name is "+person.getPerson());
+
+// const greenMsg=chalk.red('Error!...');
+// console.log(greenMsg);
+// const msg1=chalk.yellowBright('Hello', chalk.underline.blue('world') + '!')
+// console.log(msg1);
